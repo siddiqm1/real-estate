74d7873fa189f95c19ca7a06ea5c0dba784eb9a7
Muhammad Sidiq <moehammad.siddiq@gmail.com>
2025-03-17 23:16:09 +0700

    minor update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <div className="text-lg text-red-600 bg-amber-800">
-        Ini adalah Home
+      <div className="h-screen w-full 2xl:max-w-[1366px] mx-auto px-6 xl:max-w-6xl lg:max-w-4xl md:max-w-2xl">
+        <div className="">
+          <Navbar />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
